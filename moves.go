2b0d7181f9b0ca2d4f411c20f32f1a1e1ed8bd3e
pete 'Peteches' McCabe <pete@peteches.co.uk>
2022-07-14 10:28:30 +0100

    Adding Orthaganol moves funcs

--- moves.go
+++ moves.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"github.com/rs/zerolog/log"
+)
+
 // nolint:cyclop,gocognit // can't really be simplified
 func KnightMoves(src Square) []Square {
 	moves := []Square{}
@@ -42,6 +46,70 @@ func KnightMoves(src Square) []Square {
 	// nolint:gomnd // not sure how to name these as constants
 	if src.Rank() < eighthRank && src.File() < GFile {
 		moves = append(moves, src>>6)
+	}
+
+	return moves
+}
+
+func OrthaganolRankMoves(src Square) []Square {
+	log.Debug().Str("Source", boardMatrixItoS[src]).
+		Msg("Checking Destinations for Rank Moves")
+
+	moves := []Square{}
+
+	tgtSquare := src >> 1
+	for tgtSquare.Rank() == src.Rank() {
+		log.Debug().
+			Str("Source", boardMatrixItoS[src]).
+			Str("Square:", boardMatrixItoS[tgtSquare]).
+			Msg("Adding square")
+
+		moves = append(moves, tgtSquare)
+		tgtSquare >>= 1
+	}
+
+	tgtSquare = src << 1
+	for tgtSquare.Rank() == src.Rank() {
+		log.Debug().
+			Str("Source", boardMatrixItoS[src]).
+			Str("Square:", boardMatrixItoS[tgtSquare]).
+			Msg("Adding square")
+
+		moves = append(moves, tgtSquare)
+		tgtSquare <<= 1
+	}
+
+	return moves
+}
+
+func OrthaganolFileMoves(src Square) []Square {
+	log.Debug().Str("Source", boardMatrixItoS[src]).
+		Msg("Checking Destinations for File Moves")
+
+	moves := []Square{}
+
+	// nolint:gomnd // 8 is the number of squares between Ranks
+	tgtSquare := src >> 8
+	for tgtSquare.File() == src.File() {
+		log.Debug().
+			Str("Source", boardMatrixItoS[src]).
+			Str("Square:", boardMatrixItoS[tgtSquare]).
+			Msg("Adding square")
+
+		moves = append(moves, tgtSquare)
+		tgtSquare >>= 8
+	}
+
+	// nolint:gomnd // 8 is the number of squares between Ranks
+	tgtSquare = src << 8
+	for tgtSquare.File() == src.File() {
+		log.Debug().
+			Str("Source", boardMatrixItoS[src]).
+			Str("Square:", boardMatrixItoS[tgtSquare]).
+			Msg("Adding square")
+
+		moves = append(moves, tgtSquare)
+		tgtSquare <<= 8
 	}
 
 	return moves
--- moves_test.go
+++ moves_test.go
@@ -36,3 +36,57 @@ func TestKnightMoves(t *testing.T) {
 		})
 	})
 }
+
+// nolint:dupl // this could be merged with Rank Moves but prefer clarity of
+// separation.
+func TestOrthaganolRankMoves(t *testing.T) {
+	Convey("With an OrthaganolRankMoves() func", t, func() {
+		Convey("It should accept a square and return a list of valid destination moves", func() {
+			testCases := map[Square][]Square{
+				A1: {B1, C1, D1, E1, F1, G1, H1},
+				H1: {A1, B1, C1, D1, E1, F1, G1},
+				C1: {A1, B1, D1, E1, F1, G1, H1},
+				A8: {B8, C8, D8, E8, F8, G8, H8},
+				H8: {A8, B8, C8, D8, E8, F8, G8},
+				C8: {A8, B8, D8, E8, F8, G8, H8},
+				A6: {B6, C6, D6, E6, F6, G6, H6},
+				H6: {A6, B6, C6, D6, E6, F6, G6},
+				C6: {A6, B6, D6, E6, F6, G6, H6},
+			}
+			for src, expectedDsts := range testCases {
+				moves := OrthaganolRankMoves(src)
+				So(moves, ShouldHaveLength, len(expectedDsts))
+				for _, dstSqr := range expectedDsts {
+					So(moves, ShouldContain, dstSqr)
+				}
+			}
+		})
+	})
+}
+
+// nolint:dupl // this could be merged with Rank Moves but prefer clarity of
+// separation.
+func TestOrthaganolFileMoves(t *testing.T) {
+	Convey("With an OrthaganolFileMoves() func", t, func() {
+		Convey("It should accept a square and return a list of valid destination moves", func() {
+			testCases := map[Square][]Square{
+				A1: {A2, A3, A4, A5, A6, A7, A8},
+				A8: {A1, A2, A3, A4, A5, A6, A7},
+				A5: {A1, A2, A3, A4, A6, A7, A8},
+				H1: {H2, H3, H4, H5, H6, H7, H8},
+				H8: {H1, H2, H3, H4, H5, H6, H7},
+				H5: {H1, H2, H3, H4, H6, H7, H8},
+				E1: {E2, E3, E4, E5, E6, E7, E8},
+				E8: {E1, E2, E3, E4, E5, E6, E7},
+				E5: {E1, E2, E3, E4, E6, E7, E8},
+			}
+			for src, expectedDsts := range testCases {
+				moves := OrthaganolFileMoves(src)
+				So(moves, ShouldHaveLength, len(expectedDsts))
+				for _, dstSqr := range expectedDsts {
+					So(moves, ShouldContain, dstSqr)
+				}
+			}
+		})
+	})
+}
